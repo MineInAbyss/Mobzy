fe6f54df3373f3f6aced8f5bcdd2cea6d927da5c
0ffz <dan.voznyy@gmail.com>
2020-11-17 13:39:23 -0500

    Use new KotlinSpice

--- src/main/java/com/mineinabyss/mobzy/registration/MobzyTypes.kt
+++ src/main/java/com/mineinabyss/mobzy/registration/MobzyTypes.kt
@@ -8,17 +8,9 @@ import com.mineinabyss.mobzy.api.nms.aliases.NMSEntityType
 import com.mineinabyss.mobzy.api.nms.entity.typeName
 import com.mineinabyss.mobzy.mobs.CustomMob
 import com.mineinabyss.mobzy.mobs.MobType
-import com.mineinabyss.mobzy.registration.MobzyTypes.persistentTypes
 import com.mineinabyss.mobzy.registration.MobzyTypes.types
 import org.bukkit.entity.Mob
 
-/**
- * Manages registered [MobType]s and accessing them via name, bukkit entity, etc...
- *
- * @property types A map of [MobType]s registered with the plugin.
- * @property persistentTypes A map of types similar to [types], which persists (command) reloads. Useful for other
- * plugins which register types via code and can't re-register them easily.
- */
 object MobzyTypes: GearyEntityTypes<MobType>() {
     /** Gets a mob template if it is registered with the plugin, otherwise throws an [IllegalArgumentException] */
     operator fun get(type: NMSEntityType<*>): MobType = get(type.typeName)
