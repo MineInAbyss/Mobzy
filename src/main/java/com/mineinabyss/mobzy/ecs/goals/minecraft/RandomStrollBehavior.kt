704737b6599417a20fbb12c9ea69acd82fe78f9d
0ffz <dan.voznyy@gmail.com>
2021-04-09 23:56:50 -0400

    Rename some behaviours for consistency

--- src/main/java/com/mineinabyss/mobzy/ecs/goals/minecraft/RandomStrollBehavior.kt
+++ src/main/java/com/mineinabyss/mobzy/ecs/goals/minecraft/RandomStrollBehavior.kt
@@ -2,20 +2,24 @@ package com.mineinabyss.mobzy.ecs.goals.minecraft
 
 import com.mineinabyss.idofront.nms.aliases.NMSPathfinderGoal
 import com.mineinabyss.idofront.nms.aliases.toNMS
+import com.mineinabyss.idofront.time.TimeSpan
+import com.mineinabyss.idofront.time.ticks
 import com.mineinabyss.mobzy.ecs.components.initialization.pathfinding.PathfinderComponent
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import net.minecraft.server.v1_16_R2.EntityCreature
-import net.minecraft.server.v1_16_R2.PathfinderGoalPanic
+import net.minecraft.server.v1_16_R2.PathfinderGoalRandomStroll
 import org.bukkit.entity.Mob
 
 @Serializable
-@SerialName("minecraft:behavior.panic_on_hit")
-class PanicOnHitBehavior(
+@SerialName("minecraft:behavior.random_stroll")
+class RandomStrollBehavior(
     private val speedModifier: Double = 1.0,
+    private val interval: TimeSpan = 120.ticks
 ) : PathfinderComponent() {
-    override fun build(mob: Mob): NMSPathfinderGoal = PathfinderGoalPanic(
+    override fun build(mob: Mob): NMSPathfinderGoal = PathfinderGoalRandomStroll(
         mob.toNMS<EntityCreature>(),
-        speedModifier
+        speedModifier,
+        interval.inTicks.toInt()
     )
 }
