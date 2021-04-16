3a00d16aa731a2bdfee2de7a27efb63d8b5e62d5
0ffz <dan.voznyy@gmail.com>
2021-04-15 22:37:49 -0400

    Switch to Gradle Kotlin DSL, Update to Gradle 7.0

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://repo.mineinabyss.com/releases")
+    }
+}
+
+rootProject.name = "mobzy"
+
+include("processor")
