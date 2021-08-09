4e98c5630151b16822fed3718875cf9ac1190ead
0ffz <dan.voznyy@gmail.com>
2021-08-09 00:43:36 -0400

    Add worldguard dep version back

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     compileOnly("com.mineinabyss:geary-platform-papermc:0.6.49")
     compileOnly("com.mineinabyss:geary-commons-papermc:0.1.2")
     compileOnly("com.mineinabyss:protocolburrito:0.2.20")
-    compileOnly("com.sk89q.worldguard:worldguard-bukkit:") { exclude(group = "org.bukkit") }
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.2") { exclude(group = "org.bukkit") }
     compileOnly("com.comphenix.protocol:ProtocolLib:4.5.0")
     compileOnly("com.ticxo.modelengine:api:R2.1.6")
 
