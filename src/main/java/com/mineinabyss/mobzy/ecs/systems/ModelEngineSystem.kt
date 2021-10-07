ddef7a8fcf59c60927122798a45a3d76ab4cc057
0ffz <dan.voznyy@gmail.com>
2021-10-06 20:24:10 -0400

    Fix error when ModelEngine is not present

--- src/main/java/com/mineinabyss/mobzy/ecs/systems/ModelEngineSystem.kt
+++ src/main/java/com/mineinabyss/mobzy/ecs/systems/ModelEngineSystem.kt
@@ -4,13 +4,14 @@ import com.mineinabyss.geary.minecraft.events.GearyMinecraftSpawnEvent
 import com.mineinabyss.idofront.typealiases.BukkitEntity
 import com.mineinabyss.mobzy.ecs.components.initialization.ModelEngineComponent
 import com.ticxo.modelengine.api.ModelEngineAPI
+import com.ticxo.modelengine.api.manager.ModelManager
 import com.ticxo.modelengine.api.model.ModeledEntity
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 
 
 object ModelEngineSystem : Listener {
-    private val modelManager by lazy { ModelEngineAPI.api.modelManager }
+    private val modelManager: ModelManager? by lazy { ModelEngineAPI.api.modelManager }
 
     fun BukkitEntity.toModelEntity(): ModeledEntity? = modelManager.getModeledEntity(uniqueId)
 
@@ -18,9 +19,9 @@ object ModelEngineSystem : Listener {
     fun GearyMinecraftSpawnEvent.registerModelEngine() {
         val model = entity.get<ModelEngineComponent>() ?: return
         val bukkit = entity.get<BukkitEntity>() ?: return
-        val modelEntity = bukkit.toModelEntity() ?: modelManager.createModeledEntity(bukkit) ?: return
+        val modelEntity = bukkit.toModelEntity() ?: modelManager?.createModeledEntity(bukkit) ?: return
 
-        val createdModel = modelManager.createActiveModel(model.modelId)?.apply {
+        val createdModel = modelManager?.createActiveModel(model.modelId)?.apply {
             setDamageTint(model.damageTint)
         }
         modelEntity.addActiveModel(createdModel)
