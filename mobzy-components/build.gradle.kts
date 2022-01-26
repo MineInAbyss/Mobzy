5dc9cf972aa20f1f0c7770914bd582a360cca921
Nathan Harris <43653484+pixelfac@users.noreply.github.com>
2022-01-25 20:43:45 -0500

    update to new version of ticxo model engine

--- mobzy-components/build.gradle.kts
+++ mobzy-components/build.gradle.kts
@@ -16,5 +16,5 @@ dependencies {
     compileOnly(Deps.kotlinx.serialization.json)
 
     compileOnly("com.mineinabyss:geary-platform-papermc:$gearyVersion")
-    compileOnly("com.ticxo.modelengine:api:R2.2.0")
+    compileOnly("com.ticxo.modelengine:api:R2.3.1")
 }
