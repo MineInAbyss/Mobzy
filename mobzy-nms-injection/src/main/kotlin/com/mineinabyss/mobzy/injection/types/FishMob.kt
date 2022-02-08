150bec9885c1b3b100ce889cd19c71dbe9e74887
Boy <sivertpaulsen2@gmail.com>
2022-02-08 16:30:43 +0100

    improvements to fish swimming

--- mobzy-nms-injection/src/main/kotlin/com/mineinabyss/mobzy/injection/types/FishMob.kt
+++ mobzy-nms-injection/src/main/kotlin/com/mineinabyss/mobzy/injection/types/FishMob.kt
@@ -8,10 +8,13 @@ import com.mineinabyss.mobzy.injection.geary
 import com.mineinabyss.mobzy.injection.makeSound
 import net.minecraft.network.chat.IChatBaseComponent
 import net.minecraft.world.entity.EntityTypes
+import net.minecraft.world.entity.ai.control.SmoothSwimmingLookControl
+import net.minecraft.world.entity.ai.control.SmoothSwimmingMoveControl
 import net.minecraft.world.entity.animal.EntityFishSchool
+import net.minecraft.world.level.pathfinder.PathType
 
 //@GenerateFromBase(base = MobBase::class, createFor = [EntityFishSchool::class])
-open class FishMob(
+class FishMob(
     type: NMSEntityType<*>, world: NMSWorld
 ) : EntityFishSchool(type as EntityTypes<out EntityFishSchool>, world), CustomEntity {
     //bucket you get from picking up fish (we disable this interaction anyways)
@@ -24,6 +27,11 @@ open class FishMob(
     //on player interact
     override fun b(entityhuman: NMSEntityHuman, enumhand: NMSHand) = NMSInteractionResult.d //PASS
 
+    init {
+        a(PathType.i, 0.0F)
+        bM = SmoothSwimmingMoveControl(this, 85, 10, 1.0f, 0.5f, false)
+        bL = SmoothSwimmingLookControl(this, 20)
+    }
 
     override fun getScoreboardDisplayName(): IChatBaseComponent = NMSChatMessage(entityType.typeName
         .split('_').joinToString(" ") { it.replaceFirstChar(Char::uppercase) })
