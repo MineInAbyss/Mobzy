953e2b8f237bc23488c8a19894394410e198b788
Boy <sivertpaulsen2@gmail.com>
2022-02-17 22:53:13 +0100

    override axolotl-base movement

--- mobzy-nms-injection/src/main/kotlin/com/mineinabyss/mobzy/injection/types/HostileWaterMob.kt
+++ mobzy-nms-injection/src/main/kotlin/com/mineinabyss/mobzy/injection/types/HostileWaterMob.kt
@@ -8,7 +8,10 @@ import com.mineinabyss.mobzy.injection.geary
 import com.mineinabyss.mobzy.injection.makeSound
 import net.minecraft.network.chat.IChatBaseComponent
 import net.minecraft.world.entity.EntityTypes
+import net.minecraft.world.entity.ai.control.SmoothSwimmingLookControl
+import net.minecraft.world.entity.ai.control.SmoothSwimmingMoveControl
 import net.minecraft.world.entity.animal.axolotl.Axolotl
+import net.minecraft.world.level.pathfinder.PathType
 
 class HostileWaterMob(
     type: NMSEntityType<*>, world: NMSWorld
@@ -19,6 +22,12 @@ class HostileWaterMob(
 
     override fun getScoreboardDisplayName(): IChatBaseComponent = NMSChatMessage(entityType.typeName
         .split('_').joinToString(" ") { it.replaceFirstChar(Char::uppercase) })
+
+    init {
+        a(PathType.i, 0.0F)
+        bM = SmoothSwimmingMoveControl(this, 85, 10, 0.5f, 0.5f, false)
+        bL = SmoothSwimmingLookControl(this, 20)
+    }
 
     override fun getSoundVolume(): Float = geary.get<Sounds>()?.volume ?: super.getSoundVolume()
     override fun getSoundAmbient(): NMSSound? = makeSound(super.getSoundAmbient()) { ambient }
