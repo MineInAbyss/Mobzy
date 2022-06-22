9550cf53b0af513283f0e261d2c15098500251a2
Boy <sivertpaulsen2@gmail.com>
2022-06-22 16:11:01 +0200

    cancel bucketing of bucketable mobs without the component

--- mobzy-systems/src/main/kotlin/com/mineinabyss/mobzy/systems/listeners/BucketableListener.kt
+++ mobzy-systems/src/main/kotlin/com/mineinabyss/mobzy/systems/listeners/BucketableListener.kt
@@ -8,12 +8,18 @@ import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerBucketEntityEvent
 import org.bukkit.event.player.PlayerInteractEntityEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.EquipmentSlot
 import org.bukkit.inventory.ItemStack
 
 object BucketableListener : Listener {
+
+    @EventHandler
+    fun PlayerBucketEntityEvent.cancelBucketEntity() {
+        if (!entity.toGeary().has<Bucketable>()) isCancelled = true
+    }
 
     @EventHandler(ignoreCancelled = true)
     fun PlayerInteractEntityEvent.onPickupMob() {
