072cd5e0bbfa7350553c4f6481e7f42b97b3b34e
Boy0000 <sivertpaulsen2@gmail.com>
2022-08-21 03:09:01 +0200

    make tempt take serializable itemstack (#111)
    * make tempt take serializable itemstack
    
    * fix null itemstack

--- mobzy-pathfinding/src/main/kotlin/com/mineinabyss/mobzy/ecs/goals/minecraft/TemptBehavior.kt
+++ mobzy-pathfinding/src/main/kotlin/com/mineinabyss/mobzy/ecs/goals/minecraft/TemptBehavior.kt
@@ -1,30 +1,31 @@
 package com.mineinabyss.mobzy.ecs.goals.minecraft
 
 import com.mineinabyss.idofront.nms.aliases.toNMS
+import com.mineinabyss.idofront.serialization.SerializableItemStack
 import com.mineinabyss.mobzy.ecs.components.initialization.pathfinding.PathfinderComponent
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import net.minecraft.world.entity.PathfinderMob
 import net.minecraft.world.entity.ai.goal.TemptGoal
 import net.minecraft.world.item.crafting.Ingredient
-import org.bukkit.Material
+import org.bukkit.craftbukkit.v1_19_R1.inventory.CraftItemStack
 import org.bukkit.entity.Mob
 import org.bukkit.inventory.ItemStack
 
 @Serializable
 @SerialName("minecraft:behavior.tempt")
 class TemptBehavior(
-    val items: List<Material>,
-    val speed: Double = 1.0,
-    val losesInterest: Boolean = false
+    private val items: List<SerializableItemStack>,
+    private val speed: Double = 1.0,
+    private val losesInterest: Boolean = false
 ) : PathfinderComponent() {
     override fun build(mob: Mob) = TemptGoal(
         mob.toNMS<PathfinderMob>(),
         speed,
-        items.map { ItemStack(it) }.toNMSRecipeItemStack(),
+        items.map { it.toItemStack() }.toNMSRecipeItemStack(),
         losesInterest,
     )
 }
 
 fun Collection<ItemStack>.toNMSRecipeItemStack(): Ingredient =
-    Ingredient.of(stream().map { it.toNMS() })
+    Ingredient.of(stream().map { CraftItemStack.asNMSCopy(it) })
