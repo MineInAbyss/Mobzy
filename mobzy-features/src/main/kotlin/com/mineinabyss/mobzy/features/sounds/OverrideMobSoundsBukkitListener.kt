262fece07562a96728f07f53da0d2514d0697ebe
Danielle Voznyy <dan.voznyy@gmail.com>
2024-03-13 19:07:27 -0400

    Fix: No custom mob sounds playing (seems using mob as an emitter doesn't work even though the packet is sent) Fix: Damage sound playing with death sound

--- mobzy-features/src/main/kotlin/com/mineinabyss/mobzy/features/sounds/OverrideMobSoundsBukkitListener.kt
+++ mobzy-features/src/main/kotlin/com/mineinabyss/mobzy/features/sounds/OverrideMobSoundsBukkitListener.kt
@@ -5,6 +5,7 @@ import com.mineinabyss.geary.papermc.tracking.entities.toGearyOrNull
 import com.mineinabyss.idofront.typealiases.BukkitEntity
 import net.kyori.adventure.key.Key
 import net.kyori.adventure.sound.Sound
+import org.bukkit.entity.LivingEntity
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageByEntityEvent
@@ -20,20 +21,22 @@ class OverrideMobSoundsBukkitListener : Listener {
     @EventHandler(ignoreCancelled = true)
     fun EntityDamageByEntityEvent.onDamage() {
         val sounds = entity.toGearyOrNull()?.get<Sounds>() ?: return
+        if (((entity as? LivingEntity)?.health?.minus(damage) ?: 1.0) <= 0) return
         makeSound(entity, sounds.hurt)
     }
 
     companion object {
         fun makeSound(mob: BukkitEntity, sound: Sounds.Sound?) {
             if (sound == null) return
-            mob.location.world.playSound(
+            val loc = mob.location
+            loc.world.playSound(
                 Sound.sound(
                     Key.key(sound.sound),
                     sound.category,
                     sound.volume,
                     sound.adjustedPitch()
                 ),
-                mob
+                loc.x, loc.y, loc.z
             )
         }
     }
